de2cda435c3841316a045a0fc521e8ffc6d24804
khalil <khalilazennoud3@gmail.com>
2020-03-09 23:11:31 +0100

    first commit

--- testdevops/src/testdevops/Test.java
+++ testdevops/src/testdevops/Test.java
@@ -3,7 +3,7 @@ package testdevops;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.print("hello from khalil");
+		System.out.print("hello from khalil  dsd");
 	}
 
 }
